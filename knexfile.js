fe39dbc73a5d3058a3b92ef58df9f2b433987b94
Jennifer Hikichi <jenniferhikichi@Jennifers-MBP.home>
2018-11-03 15:14:13 -1000

    connection made

--- knexfile.js
+++ knexfile.js
@@ -3,9 +3,18 @@
 module.exports = {
 
   development: {
-    client: 'sqlite3',
+    client: 'postgresql',
     connection: {
-      filename: './dev.sqlite3'
+      database: process.env.POSTGRES_DB,
+      user:     process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
     }
   },
 
